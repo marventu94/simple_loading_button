d18bea5751be716c7a9fab32628bcd049e8e3ce6
Martin Marcelo Venturino <mar_venturino@hotmail.com>
2022-10-20 15:17:08 -0300

    ready for launch alpha

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -35,7 +35,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: SimpleLoadingButton(
           label: 'Pepe',
           onPressed: () async {
-            await Future.delayed(const Duration(seconds: 5));
+            await Future.delayed(const Duration(seconds: 2));
           },
           fontSize: 20,
         ),
